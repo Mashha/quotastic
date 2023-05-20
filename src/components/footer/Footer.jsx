d9980a879eb3192024823d25a3d0576a589cded1
Masa Orel <orel.masha89@gmail.com>
2023-05-20 16:33:05 +0200

    added footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+import icon from "../../assets/images/footer-icon.png";
+
+function Footer() {
+  return (
+    <footer>
+      <img src={icon} alt="" />
+      <p>All Rights Reserved | skillupmentor.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "../components/nav/Nav";
 import Hero from "../components/hero/Hero";
+import Footer from "../components/footer/Footer";
 
 function Home() {
   return (
     <>
       <Nav />
       <Hero />
+      <Footer />
     </>
   );
 }
